156295803a5f348a093abbfe86bc2c73149fcb5b
min <min.yoo@serend.io>
2019-01-23 14:13:55 -0800

    update cookbook

--- php/recipes/default.rb
+++ php/recipes/default.rb
@@ -19,30 +19,50 @@
 # limitations under the License.
 #
 
-include_recipe "php::#{node['php']['install_method']}"
+# include_recipe "php::#{node['php']['install_method']}"
 
-# update the main channels
-node['php']['pear_channels'].each do |channel|
-  php_pear_channel channel do
-    binary node['php']['pear']
-    action :update
-    only_if { node['php']['pear_setup'] }
-  end
+# # update the main channels
+# node['php']['pear_channels'].each do |channel|
+#   php_pear_channel channel do
+#     binary node['php']['pear']
+#     action :update
+#     only_if { node['php']['pear_setup'] }
+#   end
+# end
+
+# include_recipe 'php::ini'
+
+
+# ## Set package names to install PHP 7.0 on Amazon Linux
+# node.default['php']['packages'] = %w[
+#   php70 php70-common php70-curl php70-devel php70-mbstring
+#   php70-mcrypt php70-mysqlnd php70-opcache php70-pdo php7-pear
+#   php70-pecl-apcu php70-readline
+# ]
+
+# # Set PECL and PEAR binary for Amazon Linux PHP 7.0
+# node.default['php']['pear'] = 'pear7'
+# node.default['php']['pecl'] = 'pecl7'
+
+# # Run default recipe
+# include_recipe 'php'
+
+script 'add remi repo for yum' do
+    interpreter 'bash'
+    code <<-EOH
+        cd /tmp
+        wget http://rpms.remirepo.net/enterprise/remi-release-6.rpm
+        sudo rpm -Uvh remi-release*rpm
+    EOH
 end
 
-include_recipe 'php::ini'
+# Install php yaml
+script 'install php-yaml' do
+    interpreter 'bash'
+    code <<-EOH
+        sudo yum -y --enablerepo=remi-php72 install php72-php-pecl-yaml
+        sudo mv /opt/remi/php72/root/usr/lib64/php/modules/yaml.so /usr/lib64/php/7.2/modules/yaml.so
+    EOH
 
-
-## Set package names to install PHP 7.0 on Amazon Linux
-node.default['php']['packages'] = %w[
-  php70 php70-common php70-curl php70-devel php70-mbstring
-  php70-mcrypt php70-mysqlnd php70-opcache php70-pdo php7-pear
-  php70-pecl-apcu php70-readline
-]
-
-# Set PECL and PEAR binary for Amazon Linux PHP 7.0
-node.default['php']['pear'] = 'pear7'
-node.default['php']['pecl'] = 'pecl7'
-
-# Run default recipe
-include_recipe 'php'
+    not_if { ::File.exists?("/usr/lib64/php/7.2/modules/yaml.so") }
+end
