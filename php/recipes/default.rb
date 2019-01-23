9afd5b293a0f7d37e44130d6ca357f2f6c0fe7ba
min <min.yoo@serend.io>
2019-01-23 15:32:47 -0800

    update cookbook

--- php/recipes/default.rb
+++ php/recipes/default.rb
@@ -37,13 +37,13 @@
 script 'install php72' do
     interpreter 'bash'
     code <<-EOH
-        sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-		sudo yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
-		sudo yum install yum-utils
+        sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+		sudo yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+		sudo yum -y install yum-utils
 		sudo subscription-manager repos --enable=rhel-7-server-optional-rpms
 		sudo yum-config-manager --enable remi-php72
-		sudo yum update
-		sudo yum search php72 | more
-		sudo yum install php72 php72-php-fpm php72-php-gd php72-php-json php72-php-mbstring php72-php-mysqlnd php72-php-xml php72-php-xmlrpc php72-php-opcache
+		sudo yum -y update
+		sudo yum -y search php72 | more
+		sudo yum -y install php72 php72-php-fpm php72-php-gd php72-php-json php72-php-mbstring php72-php-mysqlnd php72-php-xml php72-php-xmlrpc php72-php-opcache
     EOH
 end
