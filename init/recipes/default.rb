43b2a00ebfe7e8eace317c103fcc0639b4a83ae1
min <min.yoo@serend.io>
2019-01-24 08:03:27 -0800

    update cookbook

--- init/recipes/default.rb
+++ init/recipes/default.rb
@@ -20,6 +20,6 @@ script 'amazon extras' do
 		sudo amazon-linux-extras install epel -y
 		sudo amazon-linux-extras install memcached1.5 -y
 		sudo amazon-linux-extras install php7.2 -y
-		sudo yum install php-pear php-devel gcc libzip-devel zlib-devel php-zip php-gd -y
+		sudo yum install php-pear php-devel gcc libzip-devel zlib-devel php-zip php-gd php-apc php-mcrypt php-pspell -y
 	EOH
 end
