327468e7ee924f322536db752d67dd00b4eebd61
min <min.yoo@serend.io>
2019-01-29 09:15:51 -0800

    update cookbook

--- deploy/recipes/default.rb
+++ deploy/recipes/default.rb
@@ -1,6 +1,6 @@
 include_recipe 'aws'
 
-aws_s3_file "/tmp/codebuild" do
+aws_s3_file "/tmp/codebuild/serend-dev.zip" do
   bucket "serend-codebuild-bucket"
   remote_path "serend-dev.zip"
   aws_access_key_id node[:custom_access_key]
