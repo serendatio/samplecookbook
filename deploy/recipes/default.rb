1ce2d6c77bbc3b5f88e68601f8aeb9cff05927da
min <min.yoo@serend.io>
2019-01-29 06:14:36 -0800

    update cookbook

--- deploy/recipes/default.rb
+++ deploy/recipes/default.rb
@@ -3,6 +3,6 @@ include_recipe 'aws'
 aws_s3_file "/tmp" do
   bucket "serend-codebuild-bucket"
   remote_path "serend-dev.zip"
-  aws_access_key_id "AKIAIYFNRVWFW6K3GQAA"
-  aws_secret_access_key "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"
+  aws_access_key_id node[:custom_access_key]
+  aws_secret_access_key node[:custom_secret_key]
 end
