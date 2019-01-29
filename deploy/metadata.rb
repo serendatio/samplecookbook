d6b5160008f6a19b97d0c8db4e950619f8fd12d7
min <min.yoo@serend.io>
2019-01-29 09:49:49 -0800

    update cookbook

--- deploy/metadata.rb
+++
@@ -1,7 +0,0 @@
-name        "deployfile"
-description "Deploy File From S3"
-maintainer  "Serend Software Inc"
-license     "Apache 2.0"
-version     "1.0.0"
-
-depends "aws"
--- deploy/recipes/default.rb
+++
@@ -1,8 +0,0 @@
-include_recipe 'aws'
-
-aws_s3_file "/tmp/codebuild/serend-dev.zip" do
-  bucket "serend-codebuild-bucket"
-  remote_path "serend-dev.zip"
-  aws_access_key_id node[:custom_access_key]
-  aws_secret_access_key node[:custom_secret_key]
-end
