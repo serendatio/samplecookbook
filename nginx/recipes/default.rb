b60c71c2db569096dfb685a743ed8195b297a56b
min <min.yoo@serend.io>
2019-01-30 15:40:47 -0800

    update cookbook

--- nginx/recipes/default.rb
+++ nginx/recipes/default.rb
@@ -8,7 +8,16 @@
 #  action :install
 #end
 
-service 'nginx' do
-  action [ :enable, :start ]
-end
+# service 'nginx' do
+#   action [ :enable, :start ]
+# end
 
+
+# Create directory for app's nginx config file
+directory '/etc/nginx/sites-available' do
+    owner 'root'
+    group 'root'
+    mode '0644'
+    action :create
+    recursive true
+end
