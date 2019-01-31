9b06dd49d2337587c6989274e7b0d3ac08d9b4d0
min <min.yoo@serend.io>
2019-01-31 09:14:14 -0800

    update cookbook

--- deploy/recipes/php.rb
+++ deploy/recipes/php.rb
@@ -4,14 +4,14 @@
 #
 
 include_recipe 'deploy'
-include_recipe "mod_php5_apache2"
-include_recipe "mod_php5_apache2::php"
+# include_recipe "mod_php5_apache2"
+# include_recipe "mod_php5_apache2::php"
 
 node[:deploy].each do |application, deploy|
-  if deploy[:application_type] != 'php'
-    Chef::Log.debug("Skipping deploy::php application #{application} as it is not an PHP app")
-    next
-  end
+  # if deploy[:application_type] != 'php'
+  #   Chef::Log.debug("Skipping deploy::php application #{application} as it is not an PHP app")
+  #   next
+  # end
 
   opsworks_deploy_dir do
     user deploy[:user]
