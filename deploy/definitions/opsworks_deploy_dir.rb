83029fc85cbc3f08d9b984d5f2049b743979fb4f
min <min.yoo@serend.io>
2019-01-31 10:58:16 -0800

    update cookbook

--- deploy/definitions/opsworks_deploy_dir.rb
+++ deploy/definitions/opsworks_deploy_dir.rb
@@ -4,24 +4,24 @@ define :opsworks_deploy_dir do
   # bind mounts have been made due to a race condition
   # with the automounter
 
-  if node[:opsworks_initial_setup].attribute?(:bind_mounts)
-    node[:opsworks_initial_setup][:bind_mounts][:mounts].each do |dir, source|
-      bash "Check for bind mounts before creating the deploy directory" do
-        code <<-EOH
-          retries=3
-          until grep -q #{dir} /proc/mounts; do
-            if [[ $(( retries-- )) -lt 1 ]]; then
-              echo "Bind mount check failed for #{dir}"
-              exit 1
-            fi
-            sleep 2
-          done
-        EOH
-        ignore_failure true
-        only_if { infrastructure_class?("ec2") }
-      end
-    end
-  end
+  # if node[:opsworks_initial_setup].attribute?(:bind_mounts)
+  #   node[:opsworks_initial_setup][:bind_mounts][:mounts].each do |dir, source|
+  #     bash "Check for bind mounts before creating the deploy directory" do
+  #       code <<-EOH
+  #         retries=3
+  #         until grep -q #{dir} /proc/mounts; do
+  #           if [[ $(( retries-- )) -lt 1 ]]; then
+  #             echo "Bind mount check failed for #{dir}"
+  #             exit 1
+  #           fi
+  #           sleep 2
+  #         done
+  #       EOH
+  #       ignore_failure true
+  #       only_if { infrastructure_class?("ec2") }
+  #     end
+  #   end
+  # end
 
 
   directory "#{params[:path]}/shared" do
