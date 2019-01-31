193ca65aa19e2c7542388b564d510d128835b21b
min <min.yoo@serend.io>
2019-01-30 16:09:50 -0800

    update cookbook

--- nginx/recipes/default.rb
+++ nginx/recipes/default.rb
@@ -23,49 +23,49 @@ package "nginx" do
   retry_delay 5
 end
 
-directory node[:nginx][:dir] do
-  owner 'root'
-  group 'root'
-  mode '0755'
-end
+# directory node[:nginx][:dir] do
+#   owner 'root'
+#   group 'root'
+#   mode '0755'
+# end
 
-directory node[:nginx][:log_dir] do
-  mode 0755
-  owner node[:nginx][:user]
-  action :create
-end
+# directory node[:nginx][:log_dir] do
+#   mode 0755
+#   owner node[:nginx][:user]
+#   action :create
+# end
 
-%w{sites-available sites-enabled conf.d}.each do |dir|
-  directory File.join(node[:nginx][:dir], dir) do
-    owner 'root'
-    group 'root'
-    mode '0755'
-  end
-end
+# %w{sites-available sites-enabled conf.d}.each do |dir|
+#   directory File.join(node[:nginx][:dir], dir) do
+#     owner 'root'
+#     group 'root'
+#     mode '0755'
+#   end
+# end
 
-%w{nxensite nxdissite}.each do |nxscript|
-  template "/usr/sbin/#{nxscript}" do
-    source "#{nxscript}.erb"
-    mode 0755
-    owner "root"
-    group "root"
-  end
-end
+# %w{nxensite nxdissite}.each do |nxscript|
+#   template "/usr/sbin/#{nxscript}" do
+#     source "#{nxscript}.erb"
+#     mode 0755
+#     owner "root"
+#     group "root"
+#   end
+# end
 
-template "nginx.conf" do
-  path "#{node[:nginx][:dir]}/nginx.conf"
-  source "nginx.conf.erb"
-  owner "root"
-  group "root"
-  mode 0644
-end
+# template "nginx.conf" do
+#   path "#{node[:nginx][:dir]}/nginx.conf"
+#   source "nginx.conf.erb"
+#   owner "root"
+#   group "root"
+#   mode 0644
+# end
 
-template "#{node[:nginx][:dir]}/sites-available/default" do
-  source "default-site.erb"
-  owner "root"
-  group "root"
-  mode 0644
-end
+# template "#{node[:nginx][:dir]}/sites-available/default" do
+#   source "default-site.erb"
+#   owner "root"
+#   group "root"
+#   mode 0644
+# end
 
 include_recipe "nginx::service"
 
