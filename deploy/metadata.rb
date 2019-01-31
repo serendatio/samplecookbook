931fb5fe65b592d30973ba975185994fbb574596
min <min.yoo@serend.io>
2019-01-31 10:13:35 -0800

    update cookbook

--- deploy/metadata.rb
+++ deploy/metadata.rb
@@ -4,7 +4,7 @@ maintainer  "AWS OpsWorks"
 license     "Apache 2.0"
 version     "1.0.0"
 
-depends "dependencies"
+# depends "dependencies"
 # depends "scm_helper"
 # depends "apache2"
 # depends "mod_php5_apache2"
