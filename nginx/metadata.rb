01b4f75376596415e3bb665ee996eefed269eacb
min <min.yoo@serend.io>
2019-02-01 15:12:19 -0800

    update cookbook

--- nginx/metadata.rb
+++ nginx/metadata.rb
@@ -1,84 +1,20 @@
-name        "nginx"
-description "Installs and configures nginx"
-maintainer  "AWS OpsWorks"
-license     "Apache 2.0"
-version     "1.0.0"
+name 'nginx'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'All Rights Reserved'
+description 'Installs/Configures nginx'
+long_description 'Installs/Configures nginx'
+version '0.1.0'
+chef_version '>= 13.0'
 
-attribute "nginx/dir",
-  :display_name => "Nginx Directory",
-  :description => "Location of nginx configuration files",
-  :default => "/etc/nginx"
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/nginx/issues'
 
-attribute "nginx/log_dir",
-  :display_name => "Nginx Log Directory",
-  :description => "Location for nginx logs",
-  :default => "/var/log/nginx"
-
-attribute "nginx/user",
-  :display_name => "Nginx User",
-  :description => "User nginx will run as",
-  :default => "www-data"
-
-attribute "nginx/binary",
-  :display_name => "Nginx Binary",
-  :description => "Location of the nginx server binary",
-  :default => "/usr/sbin/nginx"
-
-attribute "nginx/gzip",
-  :display_name => "Nginx Gzip",
-  :description => "Whether gzip is enabled",
-  :default => "on"
-
-attribute "nginx/gzip_http_version",
-  :display_name => "Nginx Gzip HTTP Version",
-  :description => "Version of HTTP Gzip",
-  :default => "1.0"
-
-attribute "nginx/gzip_comp_level",
-  :display_name => "Nginx Gzip Compression Level",
-  :description => "Amount of compression to use",
-  :default => "2"
-
-attribute "nginx/gzip_proxied",
-  :display_name => "Nginx Gzip Proxied",
-  :description => "Whether gzip is proxied",
-  :default => "any"
-
-attribute "nginx/gzip_types",
-  :display_name => "Nginx Gzip Types",
-  :description => "Supported MIME-types for gzip",
-  :type => "array",
-  :default => [ "text/plain", "text/html", "text/css", "application/x-javascript", "text/xml", "application/xml", "application/xml+rss", "text/javascript" ]
-
-attribute "nginx/keepalive",
-  :display_name => "Nginx Keepalive",
-  :description => "Whether to enable keepalive",
-  :default => "on"
-
-attribute "nginx/keepalive_timeout",
-  :display_name => "Nginx Keepalive Timeout",
-  :default => "65"
-
-attribute "nginx/worker_processes",
-  :display_name => "Nginx Worker Processes",
-  :description => "Number of worker processes",
-  :default => "1"
-
-attribute "nginx/worker_connections",
-  :display_name => "Nginx Worker Connections",
-  :description => "Number of connections per worker",
-  :default => "1024"
-
-attribute "nginx/server_names_hash_bucket_size",
-  :display_name => "Nginx Server Names Hash Bucket Size",
-  :default => "64"
-
-attribute "nginx/proxy_read_timeout",
-  :display_name => "Nginx Proxy Read Timeout",
-  :description => "Sets the read timeout for the response of the proxied server. It determines how long nginx will wait to get the response to a request.",
-  :default => "60"
-
-attribute "nginx/proxy_send_timeout",
-  :display_name => "Nginx Proxy Send Timeout",
-  :description => "Assigns timeout with the transfer of request to the upstream server.",
-  :default => "60"
+# The `source_url` points to the development repository for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/nginx'
--- nginx/recipes/default.rb
+++ nginx/recipes/default.rb
@@ -1,94 +1,14 @@
 #
-# Cookbook Name:: nginx
+# Cookbook:: nginx
 # Recipe:: default
-# Author:: AJ Christensen <aj@junglist.gen.nz>
 #
-# Copyright 2008, OpsCode, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# 
-#     http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# Copyright:: 2019, The Authors, All Rights Reserved.
 
-package "nginx" do
-  retries 3
-  retry_delay 5
-end
+#package 'nginx' do
+#  action :install
+#end
 
-# directory node[:nginx][:dir] do
-#   owner 'root'
-#   group 'root'
-#   mode '0755'
-# end
-
-# directory node[:nginx][:log_dir] do
-#   mode 0755
-#   owner node[:nginx][:user]
-#   action :create
-# end
-
-# %w{sites-available sites-enabled conf.d}.each do |dir|
-#   directory File.join(node[:nginx][:dir], dir) do
-#     owner 'root'
-#     group 'root'
-#     mode '0755'
-#   end
-# end
-
-# %w{nxensite nxdissite}.each do |nxscript|
-#   template "/usr/sbin/#{nxscript}" do
-#     source "#{nxscript}.erb"
-#     mode 0755
-#     owner "root"
-#     group "root"
-#   end
-# end
-
-# template "nginx.conf" do
-#   path "#{node[:nginx][:dir]}/nginx.conf"
-#   source "nginx.conf.erb"
-#   owner "root"
-#   group "root"
-#   mode 0644
-# end
-
-# template "#{node[:nginx][:dir]}/sites-available/default" do
-#   source "default-site.erb"
-#   owner "root"
-#   group "root"
-#   mode 0644
-# end
-
-include_recipe "nginx::service"
-
-# Create default nginx folder for app deployment
-directory "#{node['nginx']['deploy_dir']}" do
-  owner 'root'
-  group 'root'
-  mode '0755'
-  action :create
-end
-
-# Create default nginx folder for app deployment
-directory "/etc/nginx/sites-available" do
-  owner 'root'
-  group 'root'
-  mode '0755'
-  recursive true
-  action :create
-end
-
- 
-# Start nginx
-service "nginx" do
+service 'nginx' do
   action [ :enable, :start ]
 end
 
