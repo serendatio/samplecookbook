88d8a8a72944cb369b106f7b5ae88fd8067fbf74
min <min.yoo@serend.io>
2019-02-01 08:44:23 -0800

    update cookbook

--- deploy/recipes/default.rb
+++ deploy/recipes/default.rb
@@ -1,9 +1,28 @@
 # include_recipe 'dependencies'
+include_recipe "deploy"
+
 
 node[:deploy].each do |application, deploy|
 
   opsworks_deploy_user do
     deploy_data deploy
   end
+
+  opsworks_deploy_dir do
+    user deploy[:user]
+    group deploy[:group]
+    path deploy[:deploy_to]
+  end
+
+  opsworks_deploy do
+    app application
+    deploy_data deploy
+  end
+
+  nginx_web_app application do
+    application deploy
+  end
+
+  Chef::Log.info("Running composer update on #{deploy[:deploy_to]}")
 
 end
