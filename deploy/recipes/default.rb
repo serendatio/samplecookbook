57c6301b2475bc1d0d07f23bc2368622bf0102d2
min <min.yoo@serend.io>
2019-02-04 08:43:46 -0800

    update cookbook

--- deploy/recipes/default.rb
+++ deploy/recipes/default.rb
@@ -4,10 +4,19 @@
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
 
+include_recipe 'aws'
+
 Chef::Log.info("********* Running deploy::default ***********")
 
 node[:deploy].each do |application, deploy|
 
-	Chef::Log.info("********* #{application}, #{deploy} ***********")
+	Chef::Log.info("********* #{application}, #{deploy[:application]} ***********")
+
+	# aws_s3_file "/tmp/#{deploy[:application]}" do
+	#   bucket "test-site-config"
+	#   remote_path "vhost.map"
+	#   aws_access_key_id node[:custom_access_key]
+	#   aws_secret_access_key node[:custom_secret_key]
+	# end
 
 end
