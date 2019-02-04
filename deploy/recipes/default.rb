b905bfaa990bb540627484ec353daf8b21f29d3f
min <min.yoo@serend.io>
2019-02-04 09:02:17 -0800

    update cookbook

--- deploy/recipes/default.rb
+++ deploy/recipes/default.rb
@@ -11,7 +11,7 @@ Chef::Log.info("********* Running deploy::default ***********")
 node[:deploy].each do |application, deploy|
 
 	Chef::Log.info("********* #{application}, #{deploy[:application]} ***********")
-	Chef::Log.info("********* #{node["opsworks"]["applications"]["i"]["name"]} ***********")
+	Chef::Log.info("********* #{node[:app][:name]} ***********")
 	
 
 	# aws_s3_file "/tmp/#{deploy[:application]}" do
