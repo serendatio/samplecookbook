723dbdaedb67b3268ea0562a6c863d6f982c1983
min <min.yoo@serend.io>
2019-02-04 10:57:12 -0800

    update cookbook

--- deploy/recipes/default.rb
+++ deploy/recipes/default.rb
@@ -22,10 +22,11 @@ node[:deploy].each do |application, deploy|
 
 	aws_s3_file "/tmp/#{app['shortname']}.zip" do
 	  bucket "serend-codebuild-bucket"
-	  remote_path "#{app['app_source']['url']}"
+	  remote_path "/"
 	  aws_access_key_id "#{node['aws_access_key_id']}"
 	  aws_secret_access_key "#{node['aws_secret_access_key']}"
 	  region "us-west-2"
+	  action :create
 	end
 
 	# Chef::Log.info("********** #{aws['aws_access_key_id']} **********")
